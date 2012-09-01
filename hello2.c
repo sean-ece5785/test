db4846b50fc30a066816b05c954e36a99fbb58ec
Sean Walton <seankwalton@gmail.com>
2012-09-01 14:57:17 -0600

    one minor change

--- hello2.c
+++ hello2.c
@@ -10,6 +10,7 @@ int main(int argc, char **argv)
 {
 	printf("Hello world 2 \n");
 	printf("this file was changed in experiment branch. \n");
+	printf("changed yet again within the exp branch \n");
 	return 0;
 }
 
